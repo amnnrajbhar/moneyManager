b724154331eb59df2371b83ec114931ff2498dc4
amanrajbhar <amanrajbhar289@gmail.com>
2026-01-04 13:09:25 +0530

    bug fix

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -16,7 +16,7 @@ const auth = async (req, res, next) => {
       return res.status(401).json({ message: 'Invalid token' });
     }
 
-    req.user = { userId: user._id, ...user.toObject() };
+    req.user = user;
     next();
   } catch (error) {
     res.status(401).json({ message: 'Invalid token' });
